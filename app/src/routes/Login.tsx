0d625e2cfab7eb080dba098a66140025eade41f9
ryan-griffin <85765087+ryan-griffin@users.noreply.github.com>
2025-06-29 03:19:34 -0400

    login ui

--- app/src/routes/Login.tsx
+++ app/src/routes/Login.tsx
@@ -1,19 +1,19 @@
-import { Component, createSignal } from "solid-js";
+import { Component } from "solid-js";
+import { createStore } from "solid-js/store";
 import { useNavigate } from "@solidjs/router";
 import { useAuth } from "../components/Auth.tsx";
 import Button from "../components/Button.tsx";
+import Input from "../components/Input.tsx";
 
 const Login: Component = () => {
 	const navigate = useNavigate();
 	const { login } = useAuth();
-	const [username, setUsername] = createSignal("");
+
+	const [state, setState] = createStore({ username: "", password: "" });
 
 	const handleSubmit = async (e: Event) => {
 		e.preventDefault();
-		if (!username()) return;
-
-		const success = await login(username());
-		if (success) {
+		if (state.username && await login(state.username)) {
 			navigate("/");
 		} else {
 			alert("Login failed. Please try again.");
@@ -21,14 +21,25 @@ const Login: Component = () => {
 	};
 
 	return (
-		<form onSubmit={handleSubmit}>
-			<input
-				placeholder="Username"
-				value={username()}
-				onInput={(e) => setUsername(e.currentTarget.value)}
-			/>
-			<Button type="submit" variant="suggested" text="Login" />
-		</form>
+		<main class="flex items-center justify-center h-screen">
+			<form
+				onSubmit={handleSubmit}
+				class="flex flex-col p-8 gap-4 rounded-2xl bg-background-50 dark:bg-background-900"
+			>
+				<Input
+					placeholder="Username"
+					value={state.username}
+					onInput={(e) => setState("username", e.currentTarget.value)}
+				/>
+				<Input
+					placeholder="Password"
+					type="password"
+					value={state.password}
+					onInput={(e) => setState("password", e.currentTarget.value)}
+				/>
+				<Button type="submit" variant="suggested" text="Login" />
+			</form>
+		</main>
 	);
 };
 
